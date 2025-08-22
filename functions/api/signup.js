0248fdc4ad8a26778fa7318b462b70e63414297c
kimbrosmental <hanoiosaka1@proton.me>
2025-08-22 09:52:07 +0900

    Delete signup.js

--- functions/api/signup.js
+++
@@ -1,9 +0,0 @@
-export async function onRequestPost(context) {
-  const data = await context.request.json();
-  const kv = context.env.USERS;
-  if (await kv.get(data.username)) {
-    return new Response("이미 존재하는 아이디입니다.", { status: 409 });
-  }
-  await kv.put(data.username, JSON.stringify(data));
-  return new Response("회원가입 완료", { status: 200 });
-}
